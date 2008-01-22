945b7a9bdbd8f20c3742d072bed480d13e1ced7f
meihome <>
2008-01-22 18:14:00 +0000

    added more sample files, and create package rakefile

---
+++ sample/test/generated_sample/fruit_driver_gen.f90
@@ -0,0 +1,7 @@
+program fruit_driver_gen
+  use fruit
+  use fruit_basket_gen
+  call init_fruit
+  call fruit_basket
+  call fruit_summary
+end program fruit_driver_gen
--- sample/test/generated_sample/fruit_driver_gen_fruit.f90
+++
@@ -1,7 +0,0 @@
-program fruit_driver_gen_fruit
-  use fruit
-  use calculator_test_gen_fruit
-  call initializeFruit
-  call all_calculator_test_gen_fruit
-  call getTestSummary
-end program fruit_driver_gen_fruit
