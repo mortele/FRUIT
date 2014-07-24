ecf54230efd71dda3d1d3a71804f94460d0d94a7
istomoya <>
2014-07-24 12:53:21 +0000

    an additional test for rake_estimate.rb

---
+++ ruby_test/rake_estimate_ext_test.rb
@@ -0,0 +1,89 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require '../rake_estimate'
+require 'rubygems'
+require 'fruit_processor'
+
+class FruitRakeEstimateTest < Test::Unit::TestCase
+
+
+  def test_estim_external_func
+    dir = "external_func/"
+    esti = FruitRakeEstimate.new
+    esti.source_dirs = [dir, ]
+    esti.set_all_f
+
+    assert(esti.all_f.include?(dir + "ext_func_test.f90"), 
+      "should include #{dir}ext_func_test.f90")
+    assert(esti.all_f.include?(dir + "src_with_ext_func.f90"), 
+      "should include src_with_ext_func.f90")
+    assert(esti.all_f.include?(dir + "src_add_two.f90"), 
+      "should include src_add_two.f90"      )
+
+    forward = esti.set_forward
+    assert_equal(
+             ["src_with_ext_func.f90", "src_add_two.f90"].sort, 
+      forward["ext_func_test.f90"].sort, 
+      "ext_func_test.f90 should need src_with_ext_func.f90 and src_add_two.f90."
+    )
+  end
+
+
+  def test_estim_external_func2
+    dir = "external_func/"
+    esti = FruitRakeEstimate.new
+    esti.source_dirs = [dir, ]
+    esti.set_all_f
+
+    assert_equal(
+      [
+        "src_with_ext_func.f90", 
+        "src_add_two.f90", 
+        "ext_func_test.f90", 
+      ].sort,
+      esti.get_needed(["ext_func_test.f90"]).sort
+    )
+  end
+
+
+  def test_estim_external
+    dir = "rake_estim_external/"
+    esti = FruitRakeEstimate.new
+    esti.source_dirs = [dir, ]
+    esti.set_all_f
+
+    assert(esti.all_f.include?(dir + "file_with_ext_sub_test.f90"), 
+      "should include #{dir}file_with_ext_sub.f90")
+    assert(esti.all_f.include?(dir + "file_with_ext_sub.f90"), 
+      "should include file_with_ext_sub.f90")
+    assert(esti.all_f.include?(dir + "some_module.f90"), 
+      "should include file_with_ext_sub.f90")
+
+    forward = esti.set_forward
+    assert_equal(
+             ["some_module.f90"], 
+      forward["file_with_ext_sub.f90"], 
+      "file_with_ext_sub.f90 needs some_module.f90"
+    )
+
+    assert_equal(
+             ["file_with_ext_sub.f90", "abc_module.f90"].sort, 
+      forward["file_with_ext_sub_test.f90"].sort, 
+      "file_with_ext_sub_test.f90 needs file_with_ext_sub.f90"
+    )
+
+    assert_equal(
+      [
+        "abc_module.f90", 
+        "file_with_ext_sub.f90", 
+        "file_with_ext_sub_test.f90", 
+        "some_module.f90"
+      ].sort,
+      esti.get_needed(["file_with_ext_sub_test.f90"]).sort
+    )
+
+  end
+
+end
+
